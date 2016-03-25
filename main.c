0d3d7bf22430343e315d4dce3e8bbdfe47c2a619
nanjusoil <nanjusoil@gmail.com>
2016-03-25 22:51:05 +0800

    print all

--- main.c
+++ main.c
@@ -6,27 +6,32 @@ char dMemory[1024];
 int main()
 {
     FILE *iImg , *dImg;
-   unsigned char* iImgbuf,dImgbuf;
+   unsigned char* iImgbuf, *dImgbuf;
     int i = 0;
     int iImgLen , dImgLen;
     iImg = fopen("iimage.bin", "rb");
     dImg = fopen("dimage.bin", "rb");
     fseek(iImg, 0, SEEK_END);
     fseek(dImg, 0, SEEK_END);
-    dImgLen = ftell(iImg);
-    iImgLen = ftell(dImg);
+    dImgLen = ftell(dImg);
+    iImgLen = ftell(iImg);
 
-    iImgbuf =(char*) malloc(ftell(iImg));
-    dImgbuf =(char*) malloc(ftell(dImg));
+    iImgbuf =(unsigned char*) malloc(ftell(iImg));
+    dImgbuf =(unsigned char*) malloc(ftell(dImg));
 
     fseek(iImg, 0, SEEK_SET);
-    fseek(iImg, 0, SEEK_SET);
+    fseek(dImg, 0, SEEK_SET);
 
-    fread(iImgbuf , sizeof(char),dImgLen , iImg );
-    fread(dImgbuf , sizeof(char),iImgLen , dImg );
-    for(i = 0 ; i <ftell(iImg) ;i++)
+    fread(iImgbuf , sizeof(unsigned char),iImgLen , iImg );
+    fread(dImgbuf , sizeof(unsigned char),dImgLen , dImg );
+    for(i = 0 ; i <iImgLen ;i++)
         {
             printf("%02x" , iImgbuf[i]);
+        }
+    printf("\n" , iImgbuf[i]);
+    for(i = 0 ; i <dImgLen ;i++)
+        {
+            printf("%02x" , dImgbuf[i]);
         }
     fclose(iImg);
     fclose(dImg);
