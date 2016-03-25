682ef12a9f8f767db4c9ad35d26bf8c86220b550
nanjusoil <nanjusoil@gmail.com>
2016-03-26 01:25:29 +0800

    command writing

--- main.c
+++ main.c
@@ -1,14 +1,59 @@
 #include <stdio.h>
+#include <stdio.h>
+#define RTYPE 0
+#define add 32
+#define addu 33
+#define sub 34
+#define _and 36
+#define _or 37
+#define _xor 38
+#define nor 39
+#define nand 40
+#define slt 42
+#define sll 0
+#define srl 2
+#define sra 3
+#define jr 8
+#define addi 8
+#define addiu 9
+#define lw 35
+#define lh 33
+#define lhu 37
+#define lb 32
+#define lbu 36
+#define sw 43
+#define sh 41
+#define sb 40
+#define lui 15
+#define andi 12
+#define ori 13
+#define lhu 37
+#define nori 14
+#define slti 10
+#define beq 4
+#define bne 5
+#define bgtz 7
+#define j 2
+#define jal 3
+#define halt 63
 
-char iMemory[1024];
-char dMemory[1024];
+unsigned char iMemory[1024];
+unsigned char dMemory[1024];
+int PC;
+int reg[32];
 
+int mapopcode();
+iImageInit(unsigned char *,int );
+dImageInit(unsigned char *,int );
+int getfuct();
 int main()
 {
     FILE *iImg , *dImg;
    unsigned char* iImgbuf, *dImgbuf;
     int i = 0;
     int iImgLen , dImgLen;
+    int opcode;
+    int fuct;
     iImg = fopen("iimage.bin", "rb");
     dImg = fopen("dimage.bin", "rb");
     fseek(iImg, 0, SEEK_END);
@@ -29,12 +74,169 @@ int main()
             printf("%02x" , iImgbuf[i]);
         }
     printf("\n" , iImgbuf[i]);
-    for(i = 0 ; i <dImgLen ;i++)
+  /*  for(i = 0 ; i <dImgLen ;i++)
         {
             printf("%02x" , dImgbuf[i]);
         }
+*/
+    iImageInit(iImgbuf ,  iImgLen);
+    dImageInit(dImgbuf , dImgLen );
+   while(1){
+    opcode =mapopcode();
+    switch(opcode){
+        case RTYPE:
+            fuct =getfuct();
+            printf("%d ",fuct);
+            switch(fuct)
+            {
+            case add:
+                printf("add");
+                break;
+                case addu:
+                printf("addu");
+                break;
+                case sub:
+                printf("sub");
+                break;
+                case _and:
+                printf("_and");
+                break;
+                case _or:
+                printf("_or");
+                break;
+                case _xor:
+                printf("_xor");
+                break;
+                case nor:
+                printf("nor");
+                break;
+                case nand:
+                printf("nand");
+                break;
+                case slt:
+                printf("slt");
+                break;
+                case sll:
+                printf("sll");
+                break;
+                case srl:
+                printf("srl");
+                break;
+                case sra:
+                printf("sra");
+                break;
+                case jr:
+                printf("jr");
+                break;
+
+
+            }
+
+        break;
+        case addi:
+        printf("addi\n");
+        break;
+        case addiu:
+        printf("addiu\n");
+        break;
+        case lw:
+        printf("lw\n");
+        break;
+        case lh:
+        printf("lh\n");
+        break;
+        case lb:
+        printf("lb\n");
+        break;
+        case lbu:
+        printf("lbu\n");
+        break;
+        case sw:
+        printf("sw\n");
+        break;
+        case sh:
+        printf("sh\n");
+        break;
+        case sb:
+        printf("sb\n");
+        break;
+        case lui:
+        printf("lui\n");
+        break;
+        case andi:
+        printf("andi\n");
+        break;
+        case ori:
+        printf("ori\n");
+        break;
+        case nori:
+        printf("nori\n");
+        break;
+        case slti:
+        printf("slti\n");
+        break;
+        case beq:
+        printf("beq\n");
+        break;
+        case bne:
+        printf("bne\n");
+        break;
+        case bgtz:
+        printf("bgtz\n");
+        break;
+        case j:
+        printf("j\n");
+        break;
+        case jal:
+        printf("jal\n");
+        break;
+        case halt:
+       exit(0);
+
+    }
+    PC+=4;
+    }
     fclose(iImg);
     fclose(dImg);
 
 
+}
+iImageInit(unsigned char * iImgbuf ,int iImgLen)
+{
+    int i =0;
+    int addr;
+    for(i=0;i<4;i++)
+     PC= (PC<<8) + iImgbuf[i];
+  //  printf("%x",PC);
+    addr=PC;
+ //   printf("%d",addr);
+    for(i=8;i<iImgLen;i++)
+    iMemory[addr++]= iImgbuf[i];
+}
+dImageInit(unsigned char *dImgbuf , int dImgLen )
+{
+    int i =0;
+    int temp;
+    int addr=0;
+    for(i=0;i<4;i++)
+     temp= (temp<<8) + dImgbuf[i];
+    reg [29] =temp;
+    for(i=8;i<dImgLen;i++)
+    dMemory[addr++]= dImgbuf[i];
+
+}
+int getfuct()
+{
+    int temp=0;
+    temp = iMemory[PC+3];
+   temp=(temp<<2>>2) ;
+    return temp;
+}
+int mapopcode()
+{
+    unsigned int temp1;
+    temp1 = iMemory[PC];
+    temp1 = (temp1>>2);
+    //printf("%d" ,temp1);
+    return temp1;
 }
